717dc94abdd8da5fd50ec65637fc74b0eea4c4e7
harjotsk03 <harjotsk03@gmail.com>
2024-12-10 18:47:58 -0800

    change port for running app

--- app.py
+++ app.py
@@ -244,4 +244,5 @@ def chat():
         return jsonify({"error": "An error occurred processing your request"}), 500
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=True)
